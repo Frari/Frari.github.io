938e3738b8c78a80355ae7aad06e0356cb26ad60
Frari <francescoriccardi86@gmail.com>
2021-01-31 15:14:18 +0100

    icons home, booking award home, email form support.

--- js/main.js
+++ js/main.js
@@ -329,51 +329,55 @@ jQuery(document).ready(function($) {
 	}
 	windowScrolled();
 
-	//formspree gestore email
-	window.addEventListener("DOMContentLoaded", function() {
-
-		// get the form elements defined in your form HTML above
-		
-		var form = document.getElementById("my-form");
-		//var button = document.getElementById("my-form-button");
-		//var status = document.getElementById("my-form-status");
-	
-		// Success and Error functions for after the form is submitted
-		
-		function success() {
-		  form.reset();
-		  //button.style = "display: none ";
-		  status.innerHTML = "Thanks!";
-		}
-	
-		function error() {
-		  status.innerHTML = "Oops! There was a problem.";
-		}
-	
-		// handle the form submission event
-	
-		form.addEventListener("submit", function(ev) {
-		  ev.preventDefault();
-		  var data = new FormData(form);
-		  ajax(form.method, form.action, data, success, error);
-		});
-	  });
-	  
-	  // helper function for sending an AJAX request
-	
-	  function ajax(method, url, data, success, error) {
-		var xhr = new XMLHttpRequest();
-		xhr.open(method, url);
-		xhr.setRequestHeader("Accept", "application/json");
-		xhr.onreadystatechange = function() {
-		  if (xhr.readyState !== XMLHttpRequest.DONE) return;
-		  if (xhr.status === 200) {
-			success(xhr.response, xhr.responseType);
-		  } else {
-			error(xhr.status, xhr.response, xhr.responseType);
-		  }
-		};
-		xhr.send(data);
-	  }
-
 });
+
+//formspree gestore email
+window.addEventListener("DOMContentLoaded", function() {
+
+	// get the form elements defined in your form HTML above
+	
+	var form = document.getElementById("my-form");
+	var button = document.getElementById("form-button");
+	var statusSuccess = document.getElementById("status-success");
+	var statusError = document.getElementById("status-error");
+
+
+	// Success and Error functions for after the form is submitted
+	
+	function success() {
+	  form.reset();
+	  button.style = "display: none ";
+	  statusSuccess.style.display = "block"
+	  statusSuccess.innerHTML = "Grazie di averci contattato, le faremo sapere al più presto.";
+	}
+
+	function error() {
+		statusError.style.display = "block"
+	  statusError.innerHTML = "Oops! C'é stato un problema, riprovi a mandare il messaggio.";
+	}
+
+	// handle the form submission event
+
+	form.addEventListener("submit", function(ev) {
+	  ev.preventDefault();
+	  var data = new FormData(form);
+	  ajax(form.method, form.action, data, success, error);
+	});
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function() {
+	  if (xhr.readyState !== XMLHttpRequest.DONE) return;
+	  if (xhr.status === 200) {
+		success(xhr.response, xhr.responseType);
+	  } else {
+		error(xhr.status, xhr.response, xhr.responseType);
+	  }
+	};
+	xhr.send(data);
+  }
